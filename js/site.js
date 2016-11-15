32b24f6c24285cce3a81386163fbc212d03aa7ee
lececil <lcecil@thetus.com>
2016-11-14 20:39:06 -0800

    Adds dot navigation functionality

--- js/site.js
+++ js/site.js
@@ -12,11 +12,37 @@ $(function() {
 
 // Dot Navigation
 
-$(function () {
+jQuery(document).ready(function($){
+	var contentSections = $('.panel'),
+		  navigationItems = $('.dot-nav a');
 
-  $('.dot-nav a').click(function () {
+	updateNavigation();
+	$(window).on('scroll', function(){
+		updateNavigation();
+	});
 
-    $(this).children().addClass('active');
-
+	//smooth scroll to the section
+	navigationItems.on('click', function(event){
+      event.preventDefault();
+      smoothScroll($(this.hash));
   });
+
+	function updateNavigation() {
+		contentSections.each(function(){
+			$this = $(this);
+			var activeSection = $('.dot-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+				navigationItems.eq(activeSection).addClass('active-dot');
+			}else {
+				navigationItems.eq(activeSection).removeClass('active-dot');
+			}
+		});
+	}
+
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	600
+        );
+	}
 });
