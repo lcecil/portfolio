134609699afa0e442863c4a6398879eb80c06b2f
lececil <lcecil@thetus.com>
2017-01-08 07:54:00 -0800

    Adds in css transitions to the site.js file, pulls out some of the functions.

--- js/site.js
+++ js/site.js
@@ -1,45 +1,56 @@
+var navigationItems = $('.dot-nav a');
+var backgroundPanels = $('.background-image');
+var currentBackgroundPanel = $('.active-dot').attr('href');
+var currentFixedPanel = $('.panel').attr('class');
+
 var panelData = [
     {
-      "panel": "#home",
-      "color": "$panel-1",
+      "panel": "home",
+      "color": "rgba(36, 123, 160, 0.8)",
       "header": "UX Design",
-      "sub-header": "and Research",
-      "body": "Techniques methodologies and designs for a wide range of projects and problems."
+      "subHeader": "and Research",
+      "body": "Techniques methodologies and designs for a wide range of projects and problems.",
+      "link": "About Me"
     },
     {
       "panel": "information-architecture",
-      "color": "$panel-2",
+      "color": "rgba(245, 95, 92, 0.8)",
       "header": "Information",
-      "sub-header": "Architecture",
-      "body": "Content organization and structuring is the key to a successful application or website. Think of it as a card catalog for the 21st century."
+      "subHeader": "Architecture",
+      "body": "Content organization and structuring is the key to a successful application or website. Think of it as a card catalog for the 21st century.",
+      "link": "Read More"
     },
     {
       "panel": "research-methods",
-      "color": "$panel-3",
+      "color": "rgba(39, 48, 67, 0.8)",
       "header": "Research",
-      "sub-header": "Methods",
-      "body": "From A/B Testing to user interviews and everything in between. Choosing the right method for the job is the name of the game here."
+      "subHeader": "Methods",
+      "body": "From A/B Testing to user interviews and everything in between. Choosing the right method for the job is the name of the game here.",
+      "link": "Read More"
     },
     {
       "panel": "research-methods",
-      "color": "$panel-4",
+      "color": "rgba(237, 174, 73, 0.8)",
       "header": "Visual",
-      "sub-header": "Designs",
-      "body": "Communicating a design is as important as creating it. Using a quick sketch or pixel perfect visual design depends on what's needed to get the job done."
+      "subHeader": "Designs",
+      "body": "Communicating a design is as important as creating it. Using a quick sketch or pixel perfect visual design depends on what's needed to get the job done.",
+      "link": "Read More"
     },
     {
       "panel": "project-managment",
-      "color": "$panel-5",
+      "color": "rgba(89, 60, 132, 0.8)",
       "header": "Project",
-      "sub-header": "Management",
-      "body": "Agile, scrum, sprints, waterfall, kanban: whatever the buzzword of the day is, chances are I've probably tried it."
+      "subHeader": "Management",
+      "body": "Agile, scrum, sprints, waterfall, kanban: whatever the buzzword of the day is, chances are I've probably tried it.",
+      "link": "Read More"
     },
     {
       "panel": "development",
-      "color": "$panel-6",
+      "color": "rgba(81, 193, 175, 0.8)",
       "header": "Front-End",
-      "sub-header": "Development",
-      "body": "One of the my many hats, it ranges in size from beanie to ten gallon. It's more about the knowledge that I gain from wearing it that makes me a better designer."
+      "subHeader": "Development",
+      "body": "One of the my many hats, it ranges in size from beanie to ten gallon. It's more about the knowledge that I gain from wearing it that makes me a better designer.",
+      "link": "Read More"
     }
 ];
 
@@ -48,7 +59,8 @@ var panelData = [
 $(function() {
 
     $('.toggle-nav').click(function() {
-        $('.nav-wrap').toggleClass('open');
+        $('.nav-wrap').toggleClass('show');
+        $('header').toggleClass('show-menu');
         return false;
     });
 
@@ -59,7 +71,8 @@ $(function() {
 $(function() {
 
   $('.page-link').click(function() {
-    $('.panel').toggleClass('open');
+    $('.site-wrap').toggleClass('open');
+    $('header').toggleClass('show-page');
     return false;
   });
 
@@ -68,28 +81,40 @@ $(function() {
 // Navigation on click
 
 $(function () {
-  var navigationItems = $('.dot-nav a');
-  var backgroundPanels = $('.background-image');
-  var currentBackgroundPanel = $('.active-dot').attr('href');
-  var currentFixedPanel = $('.panel').attr('class');
 
   navigationItems.on('click', function(event) {
     var selectedDot = $(this);
     var selectedPanel = $(this.hash);
+    var panelNumber = $(this).attr('data-number');
+
     event.preventDefault();
 
     if (!selectedDot.hasClass('active-dot')) {
-      navigationItems.removeClass('active-dot');
-      selectedDot.addClass('active-dot');
-      backgroundPanels.removeClass('show');
-      selectedPanel.addClass('show');
+      changeDots(selectedDot);
+      changeBackground(selectedPanel);
+      changePanel(panelNumber);
     }
-
 
   });
 });
 
-function changePanel(panel) {
+function changeDots(selectedDot) {
+    navigationItems.removeClass('active-dot');
+    selectedDot.addClass('active-dot');
+};
+
+function changeBackground(selectedPanel) {
+  backgroundPanels.removeClass('show');
+  selectedPanel.addClass('show');
+};
+
+function changePanel(panelNumber) {
+  $('.primary-header').text(panelData[panelNumber].header);
+  $('.secondary-header').text(panelData[panelNumber].subHeader);
+  $('.page-description').text(panelData[panelNumber].body);
+  $('.panel').css('background-color', panelData[panelNumber].color);
+  $('.dot-nav a, .last-name').css('color', panelData[panelNumber].color);
+  $('.line').css('stroke', panelData[panelNumber].color);
 
 };
 
@@ -108,10 +133,3 @@ function onScroll (event) {
   }.bind(this), 1000);
 
 };
-
-function smoothScroll(target) {
-    $('body,html').animate(
-      {'scrollTop':target.offset().top},
-      600
-    );
-  };
