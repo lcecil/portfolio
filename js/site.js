4279a97a5061eea35c4413868dae65376f2c740b
lececil <lcecil@thetus.com>
2017-01-07 16:08:15 -0800

    Adding hidden content and fiddling with the json file.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,48 @@
+var panelData = [
+    {
+      "panel": "#home",
+      "color": "$panel-1",
+      "header": "UX Design",
+      "sub-header": "and Research",
+      "body": "Techniques methodologies and designs for a wide range of projects and problems."
+    },
+    {
+      "panel": "information-architecture",
+      "color": "$panel-2",
+      "header": "Information",
+      "sub-header": "Architecture",
+      "body": "Content organization and structuring is the key to a successful application or website. Think of it as a card catalog for the 21st century."
+    },
+    {
+      "panel": "research-methods",
+      "color": "$panel-3",
+      "header": "Research",
+      "sub-header": "Methods",
+      "body": "From A/B Testing to user interviews and everything in between. Choosing the right method for the job is the name of the game here."
+    },
+    {
+      "panel": "research-methods",
+      "color": "$panel-4",
+      "header": "Visual",
+      "sub-header": "Designs",
+      "body": "Communicating a design is as important as creating it. Using a quick sketch or pixel perfect visual design depends on what's needed to get the job done."
+    },
+    {
+      "panel": "project-managment",
+      "color": "$panel-5",
+      "header": "Project",
+      "sub-header": "Management",
+      "body": "Agile, scrum, sprints, waterfall, kanban: whatever the buzzword of the day is, chances are I've probably tried it."
+    },
+    {
+      "panel": "development",
+      "color": "$panel-6",
+      "header": "Front-End",
+      "sub-header": "Development",
+      "body": "One of the my many hats, it ranges in size from beanie to ten gallon. It's more about the knowledge that I gain from wearing it that makes me a better designer."
+    }
+];
+
 // Menu overlay
 
 $(function() {
@@ -23,8 +68,10 @@ $(function() {
 // Navigation on click
 
 $(function () {
-  var contentSections = $('.panel');
   var navigationItems = $('.dot-nav a');
+  var backgroundPanels = $('.background-image');
+  var currentBackgroundPanel = $('.active-dot').attr('href');
+  var currentFixedPanel = $('.panel').attr('class');
 
   navigationItems.on('click', function(event) {
     var selectedDot = $(this);
@@ -34,13 +81,19 @@ $(function () {
     if (!selectedDot.hasClass('active-dot')) {
       navigationItems.removeClass('active-dot');
       selectedDot.addClass('active-dot');
-      $('.show').removeClass('show');
+      backgroundPanels.removeClass('show');
       selectedPanel.addClass('show');
     }
+
+
   });
 });
 
-function changePanel (event) {
+function changePanel(panel) {
+
+};
+
+function onScroll (event) {
 
   if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
       console.log('up');
