f34c3e9faf575ea985c9a095fefcd8828b6cdecf
lcecil <lececil0827@gmail.com>
2017-03-27 17:26:29 -0700

    Changes port number in index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var staticDirectory = express.static('app');
 
 app.use('/', staticDirectory);
 
-app.listen(3000, function() {
-  console.log("The server is running on port 3000.")
+app.listen(80, function() {
+  console.log("The server is running on port 80.");
 });
